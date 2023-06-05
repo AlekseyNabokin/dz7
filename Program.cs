a2517e6d91188ebb677bb383d4ccd5353d95cd03
Aleksey <nighttom@mail.ru>
2023-06-05 17:10:31 +0700

    zadacha 47

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] Create2DRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().NextDouble() + new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Input number of rows ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input mini value of element");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max value of element");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = Create2DRandomArray(rows, columns, minValue, maxValue);
+
+Print2DArray(myArray);
